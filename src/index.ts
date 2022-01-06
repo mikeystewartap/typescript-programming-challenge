d7e2827960d5189f58c4f7f73ed07fe0754f5052
mikeystewartap <mikey@appointedd.com>
2022-01-06 10:34:54 +0000

    feat: question 3 wip"

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,46 @@
-const fs = require('fs');
+import { v4 as uuidv4 } from "uuid";
+
+const fs = require("fs");
 
 type WorkerInterval = {
   start: Date;
   end: Date;
-}
+};
 
 type Worker = {
   workerId: number;
-  intervals: WorkerInterval[]
-}
+  intervals: WorkerInterval[];
+};
 
 const getWorkers = (inputFilePath: string): Worker[] => {
-  const data = fs.readFileSync(inputFilePath, 'utf8')
+  const data = fs.readFileSync(inputFilePath, "utf8");
 
   return data.split("\n").map((rawWorker: string) => {
-    const workerParts = rawWorker.replace(/[\[\]']+/g,'').split("@");
+    const workerParts = rawWorker.replace(/[\[\]']+/g, "").split("@");
 
     const workerId = workerParts[0];
     const rawIntervals = workerParts[1] ? workerParts[1].split(",") : [];
 
-    return{
+    return {
       workerId,
       intervals: rawIntervals.map((interval: string) => {
         const intervalParts = interval.split("/");
         return {
           start: new Date(intervalParts[0]),
           end: new Date(intervalParts[1]),
-        }
-      })
-    }
-  })
-}
+        };
+      }),
+    };
+  });
+};
 
 export async function solveFirstQuestion(
   inputFilePath: string
 ): Promise<string> {
   const workers = getWorkers(inputFilePath);
-  const startIntervalsInMilliSeconds = workers.flatMap(worker => worker.intervals.map(interval => interval.start.getTime()));
+  const startIntervalsInMilliSeconds = workers.flatMap((worker) =>
+    worker.intervals.map((interval) => interval.start.getTime())
+  );
   const earliestStartInterval = startIntervalsInMilliSeconds.sort()[0];
   return new Date(earliestStartInterval).toISOString();
 }
@@ -45,14 +49,61 @@ export async function solveSecondQuestion(
   inputFilePath: string
 ): Promise<string> {
   const workers = getWorkers(inputFilePath);
-  const endIntervalsInMilliSeconds = workers.flatMap(worker => worker.intervals.map(interval => interval.end.getTime()));
-  const latestEndInterval = endIntervalsInMilliSeconds.sort()[endIntervalsInMilliSeconds.length - 1];
+  const endIntervalsInMilliSeconds = workers.flatMap((worker) =>
+    worker.intervals.map((interval) => interval.end.getTime())
+  );
+  const latestEndInterval =
+    endIntervalsInMilliSeconds.sort()[endIntervalsInMilliSeconds.length - 1];
   return new Date(latestEndInterval).toISOString();
 }
 
 export async function solveThirdQuestion(
   inputFilePath: string
 ): Promise<string[]> {
-  // TODO: Solve me!
+  const workers = getWorkers(inputFilePath);
+  questionThreeSolutionOne(workers);
+
   return [];
 }
+
+type IntervalCount = {
+  intervalCompoundKey: string;
+  intervalCount: number;
+};
+
+const questionThreeSolutionOne = (workers: Worker[]): [] => {
+  console.log(workers);
+
+  // keep track of the unique intervals over a list of workers
+  const intervalCounts: IntervalCount[] = [];
+
+  workers.forEach((worker) => {
+    worker.intervals.forEach((interval) => {
+      const startTimeMilliseconds = interval.start.getTime();
+      const endTimeMilliseconds = interval.start.getTime();
+      const intervalCompoundKey = `${startTimeMilliseconds}-${endTimeMilliseconds}`;
+
+      const intervalCount = intervalCounts.find(
+        (intervalCount) =>
+          intervalCount.intervalCompoundKey === intervalCompoundKey
+      );
+
+      if (intervalCount) {
+        intervalCount.intervalCount++;
+      } else {
+        intervalCounts.push({
+          intervalCompoundKey,
+          intervalCount: 1,
+        });
+      }
+    });
+  });
+
+  const intervalsWithTwoOrMoreWorkers = intervalCounts.filter(
+    (intervalCount) => intervalCount.intervalCount > 1
+  );
+
+  console.log({ intervalsWithTwoOrMoreWorkers });
+
+  return [];
+};
