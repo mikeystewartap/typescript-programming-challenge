54e3aa8336f0764358f3a9e1a6d4744d17f20798
mikeystewartap <mikey@appointedd.com>
2022-01-06 12:00:05 +0000

    feat: secret santa challenge

--- src/index.ts
+++ src/index.ts
@@ -107,3 +107,92 @@ const questionThreeSolutionOne = (workers: Worker[]): [] => {
 
   return [];
 };
+
+// Secret Santa Challenge
+
+type Person = {
+  id: string;
+  familyId: number;
+  name: string;
+  // who this person if giving a gift to
+  giftGiving?: Person;
+  // who this person is receiving a gift from
+  giftReceiving?: Person;
+};
+
+const getPeople = (inputFilePath: string): Person[] => {
+  const data = fs.readFileSync(inputFilePath, "utf8");
+  const rawFamilies = data.split("\n");
+  const people: Person[] = [];
+
+  rawFamilies.forEach((rawFamily: string, index: number) => {
+    const familyMembers = rawFamily.split(" ");
+    // using array index to make for easy debugging and comparison between families
+    const familyId = index;
+    familyMembers.forEach((name: string) => {
+      people.push({
+        id: uuidv4(),
+        familyId,
+        name,
+      });
+    });
+  });
+
+  return people;
+};
+
+type GetRandomPersonNotInFamilyOpts = {
+  people: Person[];
+  personReceiving: Person;
+};
+
+const getRandomPersonNotInFamily = ({
+  people,
+  personReceiving,
+}: GetRandomPersonNotInFamilyOpts): Person => {
+  const peopleNotAssigned = people.filter((person) => {
+    return (
+      !person.giftGiving &&
+      person.familyId !== personReceiving.familyId &&
+      person.id !== personReceiving.id
+    );
+  });
+
+  if (peopleNotAssigned.length === 1) {
+    return peopleNotAssigned[0];
+  }
+
+  const randomPersonNotAssignedIndex = Math.floor(
+    Math.random() * peopleNotAssigned.length
+  );
+  return peopleNotAssigned[randomPersonNotAssignedIndex];
+};
+
+const assignPeople = (people: Person[]): Person[] => {
+  people.forEach((person) => {
+    // randomly get a person who is not in their family and has not been assigned
+    const personToAssign = getRandomPersonNotInFamily({
+      people,
+      personReceiving: person,
+    });
+    person.giftReceiving = personToAssign;
+
+    const personGivingGift = people.find((p) => p.id === personToAssign.id);
+    if (typeof personGivingGift !== "undefined") {
+      personGivingGift.giftGiving = person;
+    }
+  });
+
+  return people;
+};
+
+export async function solveSantaQuestion(inputFilePath: string) {
+  const people = getPeople(inputFilePath);
+  const peopleAssigned = assignPeople(people);
+
+  // Output the results {gift give -> gift recevier}
+  peopleAssigned.forEach((person) => {
+    console.log(`${person.name} -> ${person.giftGiving?.name}`);
+  });
+  return [];
+}
